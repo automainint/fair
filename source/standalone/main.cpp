bdcda25f326e5dce6fe0537a8c169964eb4dc8b2
Mitya Selivanov <0x7fffff@guattari.ru>
2022-07-10 10:58:24 +0400

    test

--- source/standalone/main.cpp
+++ source/standalone/main.cpp
@@ -1,13 +1,16 @@
 #include "../fair/fair.h"
 
-#define CATCH_CONFIG_RUNNER
-#include <catch2/catch.hpp>
+#ifdef FAIR_ENABLE_TESTING
+#  define CATCH_CONFIG_RUNNER
+#  include <catch2/catch.hpp>
 
 auto run_tests(int argc, char **argv) -> int {
   return Catch::Session().run(argc, argv);
 }
+#endif
 
 auto main(int argc, char **argv) -> int {
+#ifdef FAIR_ENABLE_TESTING
   using namespace std::literals;
 
   for (int i = 0; i < argc; i++)
@@ -16,6 +19,7 @@ auto main(int argc, char **argv) -> int {
       argv[i]             = empty;
       return run_tests(argc, argv);
     }
+#endif
 
   return fair::run(argc, argv);
 }
