71134720087c35efc2d7c099d982515c4bb46cf8
Mitya Selivanov <0x7fffff@guattari.ru>
2022-07-24 11:12:07 +0400

    Refactor

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -4,7 +4,6 @@ To contribute, follow these steps:
 2.  Fork the repository.
 3.  Write tests for the issue.
 4.  Implement your solution for the issue.
-    - Follow [C++ Core Guidelines][guidelines-link].
     - Use [ClangFormat][clang-format-link] and `snake_case`.
 5.  Make sure all tests pass and cover your code.
 6.  Make a pull-request to the `dev` branch or to the corresponding feature branch if there is any.
--- source/fair/fair.c
+++ source/fair/fair.c
@@ -1,49 +1,97 @@
 #include "fair.h"
 
+#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int fair_run(int argc, char **argv) {
-  char const *input_file  = 0;
-  char const *output_file = 0;
+typedef struct {
+  bool        is_error;
+  char const *input_file;
+  char const *output_file;
+} parsed_arguments;
+
+parsed_arguments parse_arguments(int argc, char **argv) {
+  parsed_arguments result = { .is_error    = false,
+                              .input_file  = NULL,
+                              .output_file = NULL };
 
   for (int i = 0; i < argc; i++) {
-    if (strcmp("-i", argv[i]) == 0 && i + 1 < argc)
-      input_file = argv[++i];
-    else if (strcmp("-o", argv[i]) == 0 && i + 1 < argc)
-      output_file = argv[++i];
+    if (strcmp("-i", argv[i]) == 0 && i + 1 < argc) {
+      if (result.input_file != NULL)
+        result.is_error = true;
+      result.input_file = argv[++i];
+    } else if (strcmp("-o", argv[i]) == 0 && i + 1 < argc) {
+      if (result.output_file != NULL)
+        result.is_error = true;
+      result.output_file = argv[++i];
+    }
   }
 
-  FILE *in  = fopen(input_file, "r");
+  if (result.input_file == NULL)
+    result.is_error = true;
+  if (result.output_file == NULL)
+    result.is_error = true;
+  return result;
+}
 
-  if (!in)
-    return 1;
+typedef struct {
+  bool is_error;
+  bool is_main;
+} ast;
 
-  FILE *out = fopen(output_file, "w");
+FILE *read_input(parsed_arguments args) {
+  if (args.is_error)
+    return NULL;
+  return fopen(args.input_file, "r");
+}
 
-  if (!out) {
-    fclose(in);
-    return 1;
+FILE *write_output(parsed_arguments args) {
+  if (args.is_error)
+    return NULL;
+  return fopen(args.output_file, "w");
+}
+
+void close_file(FILE *f) {
+  if (f != NULL)
+    fclose(f);
+}
+
+ast parse_file(FILE *in) {
+  ast result = { .is_error = false, .is_main = false };
+
+  if (in == NULL)
+    result.is_error = true;
+  else {
+    char buf[1024] = { '\0' };
+    fread(buf, 1, 1023, in);
+
+    if (strstr(buf, "fn main") != 0)
+      result.is_main = true;
   }
 
-  char buf[1024] = { '\0' };
+  return result;
+}
 
-  int n = fread(buf, 1, 1023, in);
-
-  if (n < 0) {
-    fclose(in);
-    fclose(out);
+int print_ast(ast data, FILE *out) {
+  if (data.is_error || out == NULL)
     return 1;
-  }
-
-  if (strstr(buf, "fn main") != 0) {
+  if (data.is_main) {
     fputs("int main(int argc, char **argv) {\n", out);
     fputs("  return 0;\n", out);
     fputs("}\n", out);
   }
-
-  fclose(in);
-  fclose(out);
   return 0;
+}
+
+int fair_run(int argc, char **argv) {
+  parsed_arguments args = parse_arguments(argc, argv);
+
+  FILE *in  = read_input(args);
+  FILE *out = write_output(args);
+
+  int status = print_ast(parse_file(in), out);
+
+  close_file(in);
+  close_file(out);
+  return status;
 }
