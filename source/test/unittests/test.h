7d0b9998dd6121614fef65cc65955726f1d00fc6
Mitya Selivanov <0x7fffff@guattari.ru>
2022-07-26 07:52:47 +0400

    Update CI and test timeouts

--- source/test/unittests/test.h
+++ source/test/unittests/test.h
@@ -9,24 +9,6 @@ extern "C" {
 #include <stddef.h>
 #include <string.h>
 
-#ifdef _MSC_VER
-#  pragma section(".CRT$XCU", read)
-#  define ON_START_2(f, p)                                         \
-    static void f(void);                                           \
-    __declspec(allocate(".CRT$XCU")) void (*f##_)(void) = f;       \
-    __pragma(comment(linker, "/include:" p #f "_")) static void f( \
-        void)
-#  ifdef _WIN64
-#    define ON_START(f) ON_START_2(f, "")
-#  else
-#    define ON_START(f) ON_START_2(f, "_")
-#  endif
-#else
-#  define ON_START(f)                                 \
-    static void f(void) __attribute__((constructor)); \
-    static void f(void)
-#endif
-
 #define FAIR_TESTS_SIZE_LIMIT 0x1000
 #define FAIR_TEST_STRING_SIZE 0x100
 
@@ -46,22 +28,40 @@ struct fair_tests_list {
 
 extern struct fair_tests_list fair_tests_list;
 
+#ifdef _MSC_VER
+#  pragma section(".CRT$XCU", read)
+#  define FAIR_TEST_ON_START_2(f, p)                               \
+    static void f(void);                                           \
+    __declspec(allocate(".CRT$XCU")) void (*f##_)(void) = f;       \
+    __pragma(comment(linker, "/include:" p #f "_")) static void f( \
+        void)
+#  ifdef _WIN64
+#    define FAIR_TEST_ON_START(f) FAIR_TEST_ON_START_2(f, "")
+#  else
+#    define FAIR_TEST_ON_START(f) FAIR_TEST_ON_START_2(f, "_")
+#  endif
+#else
+#  define FAIR_TEST_ON_START(f)                       \
+    static void f(void) __attribute__((constructor)); \
+    static void f(void)
+#endif
+
 #define FAIR_TEST_CONCAT(a, b) a##b
 #define FAIR_TEST_CONCAT2(a, b) FAIR_TEST_CONCAT(a, b)
 
-#define TEST(name)                                  \
-  static void FAIR_TEST_CONCAT2(fair_test_run_, __LINE__)( \
-      ptrdiff_t, fair_test_report);                        \
-  ON_START(FAIR_TEST_CONCAT2(fair_test_case_, __LINE__)) { \
-    ptrdiff_t n = fair_tests_list.size;                    \
-    fair_tests_list.size++;                                \
-    fair_tests_list.tests[n].test_fn = FAIR_TEST_CONCAT2(  \
-        fair_test_run_, __LINE__);                         \
-    strcpy(fair_tests_list.tests[n].test_name, name);      \
-    fair_tests_list.tests[n].test_status = true;           \
-  }                                                        \
-  static void FAIR_TEST_CONCAT2(fair_test_run_, __LINE__)( \
-      ptrdiff_t        fair_test_index_,                   \
+#define TEST(name)                                                   \
+  static void FAIR_TEST_CONCAT2(fair_test_run_, __LINE__)(           \
+      ptrdiff_t, fair_test_report);                                  \
+  FAIR_TEST_ON_START(FAIR_TEST_CONCAT2(fair_test_case_, __LINE__)) { \
+    ptrdiff_t n = fair_tests_list.size;                              \
+    fair_tests_list.size++;                                          \
+    fair_tests_list.tests[n].test_fn = FAIR_TEST_CONCAT2(            \
+        fair_test_run_, __LINE__);                                   \
+    strcpy(fair_tests_list.tests[n].test_name, name);                \
+    fair_tests_list.tests[n].test_status = true;                     \
+  }                                                                  \
+  static void FAIR_TEST_CONCAT2(fair_test_run_, __LINE__)(           \
+      ptrdiff_t        fair_test_index_,                             \
       fair_test_report fair_test_report_)
 
 #define REQUIRE(ok) fair_test_report_(fair_test_index_, (ok))
